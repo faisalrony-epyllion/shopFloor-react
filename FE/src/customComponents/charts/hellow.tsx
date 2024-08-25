2e01013366ca2fd9927a7ac44b108d4c32af953d
faisalrony-epyllion <faisal.rony@epylliongroup.com>
2024-08-25 17:28:26 +0600

    first commit

---
+++ FE/src/customComponents/charts/hellow.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hellow() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default hellow
--- FE/src/customComponents/table/ProductionSummary.tsx
+++ FE/src/customComponents/table/ProductionSummary.tsx
@@ -38,7 +38,7 @@ const ProductionSummary = () => {
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="py-6 px-4 md:px-6 xl:px-7.5">
             <h4 className="text-xl font-semibold text-black dark:text-white">
-            Top 5 Production Summary
+            Top 10 Production Summary
         
             </h4>
           </div>
